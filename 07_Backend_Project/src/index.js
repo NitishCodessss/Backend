7e6f95e99156ff409b1b8fe4c7b1bf384038c6df
NitishCodessss <nitish304844@gmail.com>
2025-12-08 18:47:12 +0530

    DB connect successfull

---
+++ 07_Backend_Project/src/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDb from "./db/index.js"
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDb()
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ function dbconnect() {
+    mongoose.connect(`${process.env.MONGODB-URI}/${DB_NAME}`)
+}
+
+dbconnect() 
+
+*/
+    
+// same thing can be done like in line 4 to 11 but this is the more efficient practice to track it under try catch block and async await. 
+
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB-URI}/${DB_NAME}`) 
+//     } catch (error) {
+//         console.log("ERROR", error);
+//         throw error;
+//     }
+// } )()
